a59220a9455258e273ac3b9219f16f57cafcf1e4
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-03 21:47:17 -0400

    Commit 8: Can POST new Tweets

--- tweeter_app_api/app/controllers/tweets_controller.rb
+++ tweeter_app_api/app/controllers/tweets_controller.rb
@@ -7,4 +7,16 @@ class TweetsController < ApplicationController
         found_tweet = Tweet.find(params[:id])
         render json: {status: 200, tweet: found_tweet }
     end 
+    def create 
+        tweet = Tweet.new(tweet_params)
+    if tweet.save 
+        render(status: 201, json: {status: 201, tweet: tweet})
+    else 
+        render(status: 422, json: {status: 422, tweet: tweet.errors})
+    end 
+    end 
+    private 
+    def tweet_params 
+    params.require(:tweet).permit(:title, :content, :author)
+    end 
 end 

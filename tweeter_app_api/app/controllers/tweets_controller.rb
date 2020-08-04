718d6c8712c73a31523187a728afe869bcfba188
Andrew Culhane <andrewculhane@Andrews-MBP.fios-router.home>
2020-08-03 21:57:29 -0400

    Commit 9: Tweets update

--- tweeter_app_api/app/controllers/tweets_controller.rb
+++ tweeter_app_api/app/controllers/tweets_controller.rb
@@ -15,6 +15,11 @@ class TweetsController < ApplicationController
         render(status: 422, json: {status: 422, tweet: tweet.errors})
     end 
     end 
+    def update 
+        tweet = Tweet.find(params[:id])
+        tweet.update(tweet_params)
+        render(status: 201, json: {status: 201, tweet: tweet})
+    end 
     private 
     def tweet_params 
     params.require(:tweet).permit(:title, :content, :author)
